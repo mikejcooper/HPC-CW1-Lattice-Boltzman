50245fae933453fc9ed949c4f73524976ad36bfd
Mike Cooper <mikecooper@Mikes-Mac.local>
2016-10-15 14:54:09 +0100

    equation simplfy

--- d2q9-bgk.c
+++ d2q9-bgk.c
@@ -332,40 +332,40 @@ void collision(const t_param params, t_speed* cells, t_speed* tmp_cells, int* ob
        
 
         cells[index].speeds[0] = tmp_cells[index].speeds[0]
-                                                  + params.omega
-                                                  * (w0 * local_density * (1.0 - u_sq / (c_sq + c_sq))
+                                                  + params.omega 
+                                                  * (4.0 * local_density / 9.0 * (1.0 - u_sq * 1.5)
                                                     - tmp_cells[index].speeds[0]);
         cells[index].speeds[1] = tmp_cells[index].speeds[1]
                                                   + params.omega
-                                                  * (w1 * local_density * (1.0 + u_x / c_sq + (u_x * u_x) / e1 - e2)
+                                                  * (local_density / 9.0 * (1.0 + u_x * 3.0 + (u_x * u_x) * 0.5 * 9.0 - (1.5 * u_sq))
                                                     - tmp_cells[index].speeds[1]);
         cells[index].speeds[2] = tmp_cells[index].speeds[2]
                                                   + params.omega
-                                                  * (w1 * local_density * (1.0 + u_y / c_sq + (u_y * u_y) / e1 - e2)
+                                                  * (local_density / 9.0 * (1.0 + u_y * 3.0 + (u_y * u_y) * 0.5 * 9.0 - (1.5 * u_sq))
                                                     - tmp_cells[index].speeds[2]);
         cells[index].speeds[3] = tmp_cells[index].speeds[3]
                                                   + params.omega
-                                                  * (w1 * local_density * (1.0 + - u_x / c_sq + (- u_x * - u_x) / e1 - e2)
+                                                  * (local_density / 9.0 * (1.0 + - u_x * 3.0 + (- u_x * - u_x) * 0.5 * 9.0 - (1.5 * u_sq))
                                                     - tmp_cells[index].speeds[3]);
         cells[index].speeds[4] = tmp_cells[index].speeds[4]
                                                   + params.omega
-                                                  * (w1 * local_density * (1.0 + - u_y / c_sq + (- u_y * - u_y) / e1 - e2)
+                                                  * (local_density / 9.0 * (1.0 + - u_y * 3.0 + (- u_y * - u_y) * 0.5 * 9.0 - (1.5 * u_sq))
                                                     - tmp_cells[index].speeds[4]);
         cells[index].speeds[5] = tmp_cells[index].speeds[5]
                                                   + params.omega
-                                                  * (w2 * local_density * (1.0 + (u_x + u_y) / c_sq + ((u_x + u_y) * (u_x + u_y)) / e1 - e2)
+                                                  * (local_density / 36.0 * (1.0 + (u_x + u_y) * 3.0 + ((u_x + u_y) * (u_x + u_y)) * 0.5 * 9.0 - (1.5 * u_sq))
                                                     - tmp_cells[index].speeds[5]);
         cells[index].speeds[6] = tmp_cells[index].speeds[6]
                                                   + params.omega
-                                                  * (w2 * local_density * (1.0 + (- u_x + u_y) / c_sq + ((- u_x + u_y) * (- u_x + u_y)) / e1 - e2)
+                                                  * (local_density / 36.0 * (1.0 + (- u_x + u_y) * 3.0 + ((- u_x + u_y) * (- u_x + u_y)) * 0.5 * 9.0 - (1.5 * u_sq))
                                                     - tmp_cells[index].speeds[6]);
         cells[index].speeds[7] = tmp_cells[index].speeds[7]
                                                   + params.omega
-                                                  * (w2 * local_density * (1.0 + (- u_x - u_y) / c_sq + ((- u_x - u_y) * (- u_x - u_y)) / e1 - e2)
+                                                  * (local_density / 36.0 * (1.0 + (- u_x - u_y) * 3.0 + ((- u_x - u_y) * (- u_x - u_y)) * 0.5 * 9.0 - (1.5 * u_sq))
                                                     - tmp_cells[index].speeds[7]);
         cells[index].speeds[8] = tmp_cells[index].speeds[8]
                                                   + params.omega
-                                                  * (w2 * local_density * (1.0 + (u_x - u_y) / c_sq + ((u_x - u_y) * (u_x - u_y)) / e1 - e2)
+                                                  * (local_density / 36.0 * (1.0 + (u_x - u_y) * 3.0 + ((u_x - u_y) * (u_x - u_y)) * 0.5 * 9.0 - (1.5 * u_sq))
                                                     - tmp_cells[index].speeds[8]);
 
 // --------------av_velocity-----------------------------------------------
@@ -398,10 +398,6 @@ void collision(const t_param params, t_speed* cells, t_speed* tmp_cells, int* ob
         tot_u += sqrt((u_x * u_x) + (u_y * u_y));
         /* increase counter of inspected cells */
         ++tot_cells;
-
-
-
-
       }
     }
   }
