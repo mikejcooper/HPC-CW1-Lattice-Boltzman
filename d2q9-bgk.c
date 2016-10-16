30af688216175802ac58ffc4635896f5f316e230
Mike Cooper <mikecooper@Mikes-Mac.local>
2016-10-16 21:45:47 +0100

    test

--- d2q9-bgk.c
+++ d2q9-bgk.c
@@ -191,6 +191,7 @@ void accelerate_flow(t_speed* cells, int* obstacles)
   // array indexing: 16128 -> 16255
   for (int jj = 0; jj < 128; jj++)
   {
+    // int index = 126 * 128 + jj;
     int index = 254 * 128 + jj;
     /* if the cell is not occupied and
     ** we don't send a negative density */
@@ -216,6 +217,7 @@ void accelerate_flow(t_speed* cells, int* obstacles)
 void propagate(t_speed* cells, t_speed* tmp_cells)
 {
   /* loop over _all_ cells */
+  // for (int ii = 0; ii < 128; ii++)
   for (int ii = 0; ii < 256; ii++)
   {
     /* determine indices of axis-direction neighbours
@@ -258,6 +260,7 @@ void collision(t_speed* cells, t_speed* tmp_cells, int* obstacles, double* av_ve
   ** are in the scratch-space grid */
 
   for (int ii = 0; ii < 256; ii++)
+  // for (int ii = 0; ii < 128; ii++)
   {
     for (int jj = 0; jj < 128; jj++)
     {
@@ -308,6 +311,7 @@ void collision(t_speed* cells, t_speed* tmp_cells, int* obstacles, double* av_ve
                          + tmp_cells[index].speeds[8]))
                      * local_density_invert;
        
+       double e1 = d1 * (u_x * u_x + u_y * u_y);
 
         cells[index].speeds[0] = tmp_cells[index].speeds[0]
                                                   + 1.85 
@@ -315,35 +319,35 @@ void collision(t_speed* cells, t_speed* tmp_cells, int* obstacles, double* av_ve
                                                     - tmp_cells[index].speeds[0]);
         cells[index].speeds[1] = tmp_cells[index].speeds[1]
                                                   + 1.85
-                                                  * (local_density * d1 * (4 + u_x * 12 + (u_x * u_x) * 648 * d1- (216 * d1 * (u_x * u_x + u_y * u_y)))
+                                                  * (local_density * d1 * (4 + u_x * 12 + (u_x * u_x) * 648 * d1- (216 * e1))
                                                     - tmp_cells[index].speeds[1]);
         cells[index].speeds[2] = tmp_cells[index].speeds[2]
                                                   + 1.85
-                                                  * (local_density * d1 * (4 + u_y * 12 + (u_y * u_y) * 648 * d1 - (216 * d1 * (u_x * u_x + u_y * u_y)))
+                                                  * (local_density * d1 * (4 + u_y * 12 + (u_y * u_y) * 648 * d1 - (216 * e1))
                                                     - tmp_cells[index].speeds[2]);
         cells[index].speeds[3] = tmp_cells[index].speeds[3]
                                                   + 1.85
-                                                  * (local_density * d1 * (4 + - u_x * 12 + (- u_x * - u_x) * 648 * d1 - (216 * d1 * (u_x * u_x + u_y * u_y)))
+                                                  * (local_density * d1 * (4 + - u_x * 12 + (- u_x * - u_x) * 648 * d1 - (216 * e1))
                                                     - tmp_cells[index].speeds[3]);
         cells[index].speeds[4] = tmp_cells[index].speeds[4]
                                                   + 1.85
-                                                  * (local_density * d1 * (4 + - u_y * 12 + (- u_y * - u_y) * 648 * d1 - (216 * d1 * (u_x * u_x + u_y * u_y)))
+                                                  * (local_density * d1 * (4 + - u_y * 12 + (- u_y * - u_y) * 648 * d1 - (216 * e1))
                                                     - tmp_cells[index].speeds[4]);
         cells[index].speeds[5] = tmp_cells[index].speeds[5]
                                                   + 1.85
-                                                  * (local_density * d1 * (1 + (u_x + u_y) * 3 + ((u_x + u_y) * (u_x + u_y)) * 162 * d1 - (54 * d1 * (u_x * u_x + u_y * u_y)))
+                                                  * (local_density * d1 * (1 + (u_x + u_y) * 3 + ((u_x + u_y) * (u_x + u_y)) * 162 * d1 - (54 * e1))
                                                     - tmp_cells[index].speeds[5]);
         cells[index].speeds[6] = tmp_cells[index].speeds[6]
                                                   + 1.85
-                                                  * (local_density * d1 * (1 + (- u_x + u_y) * 3 + ((- u_x + u_y) * (- u_x + u_y)) * 162 * d1 - (54 * d1 * (u_x * u_x + u_y * u_y)))
+                                                  * (local_density * d1 * (1 + (- u_x + u_y) * 3 + ((- u_x + u_y) * (- u_x + u_y)) * 162 * d1 - (54 * e1))
                                                     - tmp_cells[index].speeds[6]);
         cells[index].speeds[7] = tmp_cells[index].speeds[7]
                                                   + 1.85
-                                                  * (local_density * d1 * (1 + (- u_x - u_y) * 3 + ((- u_x - u_y) * (- u_x - u_y)) * 162 * d1 - (54 * d1 * (u_x * u_x + u_y * u_y)))
+                                                  * (local_density * d1 * (1 + (- u_x - u_y) * 3 + ((- u_x - u_y) * (- u_x - u_y)) * 162 * d1 - (54 * e1))
                                                     - tmp_cells[index].speeds[7]);
         cells[index].speeds[8] = tmp_cells[index].speeds[8]
                                                   + 1.85
-                                                  * (local_density * d1 * (1 + (u_x - u_y) * 3 + ((u_x - u_y) * (u_x - u_y)) * 162 * d1 - (54 * d1 * (u_x * u_x + u_y * u_y)))
+                                                  * (local_density * d1 * (1 + (u_x - u_y) * 3 + ((u_x - u_y) * (u_x - u_y)) * 162 * d1 - (54 * e1))
                                                     - tmp_cells[index].speeds[8]);
 
 // --------------av_velocity-----------------------------------------------
