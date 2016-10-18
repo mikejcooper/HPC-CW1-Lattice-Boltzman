a333718d33fafd354b4992200c70fcc03c41e6e2
Mike Cooper <mikecooper@Mikes-Mac.local>
2016-10-18 19:03:40 +0100

    test

--- d2q9-bgk.c
+++ d2q9-bgk.c
@@ -282,12 +282,11 @@ void collision(const t_param params, t_speed* cells, t_speed* tmp_cells, int* ob
   ** the propagate step and so values of interest
   ** are in the scratch-space grid */
 // #pragma omp parallel for schedule(dynamic,1) reduction(+: tot_u, tot_cells)
- #pragma omp for
+#pragma omp parallel for
 for (int ii = 0; ii < params.ny; ii++)
   {
     int y_s = (ii == 0) ? (ii + params.ny - 1) : (ii - 1); // could move up
     int y_n = (ii + 1) % params.ny; // Could move up
-        printf("Thread %d working on data ii =  %d\n", omp_get_thread_num(), ii);
 
     for (int jj = 0; jj < params.nx; jj++)
     {
