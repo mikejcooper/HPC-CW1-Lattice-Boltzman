8d8c29d1844a1e00abcbaf7631c6a4913a4b322e
Mike Cooper <mikecooper@Mikes-Mac.local>
2016-10-19 23:06:49 +0100

    test

--- d2q9-bgk.c
+++ d2q9-bgk.c
@@ -301,14 +301,14 @@ for (int ii = 0; ii < params.ny; ii++)
       {
         /* called after propagate, so taking values from scratch space
         ** mirroring, and writing into main grid */
-        cells[index].speeds[1] = cells[ii * params.nx + x_e].speeds[3];
-        cells[index].speeds[2] = cells[y_n * params.nx + jj].speeds[4];
-        cells[index].speeds[3] = cells[ii * params.nx + x_w].speeds[1];
-        cells[index].speeds[4] = cells[y_s * params.nx + jj].speeds[2];
-        cells[index].speeds[5] = cells[y_n * params.nx + x_e].speeds[7];
-        cells[index].speeds[6] = cells[y_n * params.nx + x_w].speeds[8];
-        cells[index].speeds[7] = cells[y_s * params.nx + x_w].speeds[5];
-        cells[index].speeds[8] = cells[y_s * params.nx + x_e].speeds[6];
+        tmp_cells[index].speeds[1] = cells[ii * params.nx + x_e].speeds[3];
+        tmp_cells[index].speeds[2] = cells[y_n * params.nx + jj].speeds[4];
+        tmp_cells[index].speeds[3] = cells[ii * params.nx + x_w].speeds[1];
+        tmp_cells[index].speeds[4] = cells[y_s * params.nx + jj].speeds[2];
+        tmp_cells[index].speeds[5] = cells[y_n * params.nx + x_e].speeds[7];
+        tmp_cells[index].speeds[6] = cells[y_n * params.nx + x_w].speeds[8];
+        tmp_cells[index].speeds[7] = cells[y_s * params.nx + x_w].speeds[5];
+        tmp_cells[index].speeds[8] = cells[y_s * params.nx + x_e].speeds[6];
       } 
 // ----------------END--------------------------------------------
       else 
@@ -347,44 +347,42 @@ for (int ii = 0; ii < params.ny; ii++)
                      * local_density_invert;
 
 
-
-        cells[index].speeds[0] = cells[ii * params.nx + jj].speeds[0]                   
+tmp_cells[index].speeds[0] = cells[ii * params.nx + jj].speeds[0]                   
                                                   + params.omega 
                                                   * (local_density * d1 * (16 - (u_x * u_x + u_y * u_y) * 864 * d1)
                                                     - cells[ii * params.nx + jj].speeds[0]);
-        cells[index].speeds[1] = cells[ii * params.nx + x_w].speeds[1]
+        tmp_cells[index].speeds[1] = cells[ii * params.nx + x_w].speeds[1]
                                                   + params.omega
-                                                  * (local_density * d1 * (4 + u_x * 12 + (u_x * u_x) * 648 * d1- (216 * d1 * (u_x * u_x + u_y * u_y)))
+                                                  * (local_density * d1 * (4 + u_x * (12 + 432 * d1 * u_x) - 216 * d1 * u_y * u_y)
                                                     - cells[ii * params.nx + x_w].speeds[1]);
-        cells[index].speeds[2] = cells[y_s * params.nx + jj].speeds[2]
+        tmp_cells[index].speeds[2] = cells[y_s * params.nx + jj].speeds[2]
                                                   + params.omega
                                                   * (local_density * d1 * (4 + u_y * 12 + (u_y * u_y) * 648 * d1 - (216 * d1 * (u_x * u_x + u_y * u_y)))
                                                     - cells[y_s * params.nx + jj].speeds[2]);
-        cells[index].speeds[3] = cells[ii * params.nx + x_e].speeds[3]
+        tmp_cells[index].speeds[3] = cells[ii * params.nx + x_e].speeds[3]
                                                   + params.omega
                                                   * (local_density * d1 * (4 + - u_x * 12 + (- u_x * - u_x) * 648 * d1 - (216 * d1 * (u_x * u_x + u_y * u_y)))
                                                     - cells[ii * params.nx + x_e].speeds[3]);
-        cells[index].speeds[4] = cells[y_n * params.nx + jj].speeds[4]
+        tmp_cells[index].speeds[4] = cells[y_n * params.nx + jj].speeds[4]
                                                   + params.omega
                                                   * (local_density * d1 * (4 + - u_y * 12 + (- u_y * - u_y) * 648 * d1 - (216 * d1 * (u_x * u_x + u_y * u_y)))
                                                     - cells[y_n * params.nx + jj].speeds[4]);
-        cells[index].speeds[5] = cells[y_s * params.nx + x_w].speeds[5]
+        tmp_cells[index].speeds[5] = cells[y_s * params.nx + x_w].speeds[5]
                                                   + params.omega
                                                   * (local_density * d1 * (1 + (u_x + u_y) * 3 + ((u_x + u_y) * (u_x + u_y)) * 162 * d1 - (54 * d1 * (u_x * u_x + u_y * u_y)))
                                                     - cells[y_s * params.nx + x_w].speeds[5]);
-        cells[index].speeds[6] = cells[y_s * params.nx + x_e].speeds[6]
+        tmp_cells[index].speeds[6] = cells[y_s * params.nx + x_e].speeds[6]
                                                   + params.omega
                                                   * (local_density * d1 * (1 + (- u_x + u_y) * 3 + ((- u_x + u_y) * (- u_x + u_y)) * 162 * d1 - (54 * d1 * (u_x * u_x + u_y * u_y)))
                                                     - cells[y_s * params.nx + x_e].speeds[6]);
-        cells[index].speeds[7] = cells[y_n * params.nx + x_e].speeds[7]
+        tmp_cells[index].speeds[7] = cells[y_n * params.nx + x_e].speeds[7]
                                                   + params.omega
                                                   * (local_density * d1 * (1 + (- u_x - u_y) * 3 + ((- u_x - u_y) * (- u_x - u_y)) * 162 * d1 - (54 * d1 * (u_x * u_x + u_y * u_y)))
                                                     - cells[y_n * params.nx + x_e].speeds[7]);
-        cells[index].speeds[8] = cells[y_n * params.nx + x_w].speeds[8]
+        tmp_cells[index].speeds[8] = cells[y_n * params.nx + x_w].speeds[8]
                                                   + params.omega
                                                   * (local_density * d1 * (1 + (u_x - u_y) * 3 + ((u_x - u_y) * (u_x - u_y)) * 162 * d1 - (54 * d1 * (u_x * u_x + u_y * u_y)))
                                                     - cells[y_n * params.nx + x_w].speeds[8]);
-
 // --------------av_velocity-----------------------------------------------
 
         /* accumulate the norm of x- and y- velocity components */
