2c520861432527f3c2e798816fb42432b12db7fc
Mike Cooper <mikecooper@eduroam-90-129.nomadic.bris.ac.uk>
2016-10-20 11:41:58 +0100

    test

--- d2q9-bgk.c
+++ d2q9-bgk.c
@@ -281,7 +281,7 @@ void collision(const t_param params, t_speed* cells, t_speed* tmp_cells, int* ob
   ** the propagate step and so values of interest
   ** are in the scratch-space grid */
 // #pragma omp parallel for schedule(dynamic,1) reduction(+: tot_u, tot_cells)
-#pragma omp parallel for simd reduction(+:tot_cells,tot_u) schedule(static) num_threads(16)
+#pragma omp parallel for simd reduction(+:tot_cells,tot_u) schedule(static) num_threads(16) ivdep
 for (int ii = 0; ii < params.ny; ii++)
   {
       int y_s = (ii == 0) ? (ii + params.ny - 1) : (ii - 1); // could move up
@@ -355,26 +355,32 @@ for (int ii = 0; ii < params.ny; ii++)
         tmp_cells[index].speeds[1] = cells[ii * params.nx + x_w].speeds[1]
                                                   + params.omega
                                                   * (local_density * d1 * (4 + u_x * 12 + (u_x * u_x) * 648 * d1- (216 * d1 * (u_x * u_x + u_y * u_y)))
+                                                                        * (4 + u_x * (432 * d1 * u_x + 12) - 216 * d1 * u_y * u_y)
                                                     - cells[ii * params.nx + x_w].speeds[1]);
         tmp_cells[index].speeds[2] = cells[y_s * params.nx + jj].speeds[2]
                                                   + params.omega
                                                   * (local_density * d1 * (4 + u_y * 12 + (u_y * u_y) * 648 * d1 - (216 * d1 * (u_x * u_x + u_y * u_y)))
+                                                                        * (4 + u_y * (432 * d1 * u_y + 12) - 216 * d1 * u_x * u_x)
                                                     - cells[y_s * params.nx + jj].speeds[2]);
         tmp_cells[index].speeds[3] = cells[ii * params.nx + x_e].speeds[3]
                                                   + params.omega
                                                   * (local_density * d1 * (4 + - u_x * 12 + (- u_x * - u_x) * 648 * d1 - (216 * d1 * (u_x * u_x + u_y * u_y)))
+                                                                        * (4 + u_x * (432 * d1 * u_x - 12) - 216 * d1 * u_y * u_y)
                                                     - cells[ii * params.nx + x_e].speeds[3]);
         tmp_cells[index].speeds[4] = cells[y_n * params.nx + jj].speeds[4]
                                                   + params.omega
                                                   * (local_density * d1 * (4 + - u_y * 12 + (- u_y * - u_y) * 648 * d1 - (216 * d1 * (u_x * u_x + u_y * u_y)))
+                                                                        * (4 + u_y * (432 * d1 * u_y - 12) - 216 * d1 * u_x * u_x)
                                                     - cells[y_n * params.nx + jj].speeds[4]);
         tmp_cells[index].speeds[5] = cells[y_s * params.nx + x_w].speeds[5]
                                                   + params.omega
                                                   * (local_density * d1 * (1 + (u_x + u_y) * 3 + ((u_x + u_y) * (u_x + u_y)) * 162 * d1 - (54 * d1 * (u_x * u_x + u_y * u_y)))
+                                                                          (1 + 3 * (u_x + u_y) * (36 * d1 * (u_x + 2 * u_y) + 1))
                                                     - cells[y_s * params.nx + x_w].speeds[5]);
         tmp_cells[index].speeds[6] = cells[y_s * params.nx + x_e].speeds[6]
                                                   + params.omega
                                                   * (local_density * d1 * (1 + (- u_x + u_y) * 3 + ((- u_x + u_y) * (- u_x + u_y)) * 162 * d1 - (54 * d1 * (u_x * u_x + u_y * u_y)))
+
                                                     - cells[y_s * params.nx + x_e].speeds[6]);
         tmp_cells[index].speeds[7] = cells[y_n * params.nx + x_e].speeds[7]
                                                   + params.omega
@@ -388,7 +394,7 @@ for (int ii = 0; ii < params.ny; ii++)
 // --------------av_velocity-----------------------------------------------
 
         /* accumulate the norm of x- and y- velocity components */
-        tot_u += sqrt((u_x * u_x) + (u_y * u_y));
+        tot_u += sqrt((u_x * u_x) + (u_y * u_y)); // move might make FASTER
         /* increase counter of inspected cells */
         ++tot_cells;
       }
