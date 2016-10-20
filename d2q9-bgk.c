85e8f527089da030f038cd9ead5dd652d601c3bc
Mike Cooper <mikecooper@eduroam-90-129.nomadic.bris.ac.uk>
2016-10-20 11:54:54 +0100

    test

--- d2q9-bgk.c
+++ d2q9-bgk.c
@@ -208,7 +208,7 @@ void accelerate_flow(const t_param params, t_speed* cells, int* obstacles)
     /* if the cell is not occupied and
     ** we don't send a negative density */
     if (!obstacles[index]
-        && (cells[index].speeds[3] - 4.0 * w2) > 0.0
+        && (cells[index].speeds[3] - w1) > 0.0
         && (cells[index].speeds[6] - w2) > 0.0
         && (cells[index].speeds[7] - w2) > 0.0)
     {
@@ -356,32 +356,26 @@ for (int ii = 0; ii < params.ny; ii++)
         tmp_cells[index].speeds[1] = cells[ii * params.nx + x_w].speeds[1]
                                                   + params.omega
                                                   * (local_density * d1 * (4 + u_x * 12 + (u_x * u_x) * 648 * d1- (216 * d1 * (u_x * u_x + u_y * u_y)))
-                                                                        * (4 + u_x * (432 * d1 * u_x + 12) - 216 * d1 * u_y * u_y)
                                                     - cells[ii * params.nx + x_w].speeds[1]);
         tmp_cells[index].speeds[2] = cells[y_s * params.nx + jj].speeds[2]
                                                   + params.omega
                                                   * (local_density * d1 * (4 + u_y * 12 + (u_y * u_y) * 648 * d1 - (216 * d1 * (u_x * u_x + u_y * u_y)))
-                                                                        * (4 + u_y * (432 * d1 * u_y + 12) - 216 * d1 * u_x * u_x)
                                                     - cells[y_s * params.nx + jj].speeds[2]);
         tmp_cells[index].speeds[3] = cells[ii * params.nx + x_e].speeds[3]
                                                   + params.omega
                                                   * (local_density * d1 * (4 + - u_x * 12 + (- u_x * - u_x) * 648 * d1 - (216 * d1 * (u_x * u_x + u_y * u_y)))
-                                                                        * (4 + u_x * (432 * d1 * u_x - 12) - 216 * d1 * u_y * u_y)
                                                     - cells[ii * params.nx + x_e].speeds[3]);
         tmp_cells[index].speeds[4] = cells[y_n * params.nx + jj].speeds[4]
                                                   + params.omega
                                                   * (local_density * d1 * (4 + - u_y * 12 + (- u_y * - u_y) * 648 * d1 - (216 * d1 * (u_x * u_x + u_y * u_y)))
-                                                                        * (4 + u_y * (432 * d1 * u_y - 12) - 216 * d1 * u_x * u_x)
                                                     - cells[y_n * params.nx + jj].speeds[4]);
         tmp_cells[index].speeds[5] = cells[y_s * params.nx + x_w].speeds[5]
                                                   + params.omega
                                                   * (local_density * d1 * (1 + (u_x + u_y) * 3 + ((u_x + u_y) * (u_x + u_y)) * 162 * d1 - (54 * d1 * (u_x * u_x + u_y * u_y)))
-                                                                          (1 + 3 * (u_x + u_y) * (36 * d1 * (u_x + 2 * u_y) + 1))
                                                     - cells[y_s * params.nx + x_w].speeds[5]);
         tmp_cells[index].speeds[6] = cells[y_s * params.nx + x_e].speeds[6]
                                                   + params.omega
                                                   * (local_density * d1 * (1 + (- u_x + u_y) * 3 + ((- u_x + u_y) * (- u_x + u_y)) * 162 * d1 - (54 * d1 * (u_x * u_x + u_y * u_y)))
-
                                                     - cells[y_s * params.nx + x_e].speeds[6]);
         tmp_cells[index].speeds[7] = cells[y_n * params.nx + x_e].speeds[7]
                                                   + params.omega
