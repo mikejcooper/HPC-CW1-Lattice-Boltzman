afabd86eeedbf8a72d857fdd46c69d7885e9dd19
Mike Cooper <mikecooper@eduroam-90-129.nomadic.bris.ac.uk>
2016-10-20 12:09:35 +0100

    test

--- d2q9-bgk.c
+++ d2q9-bgk.c
@@ -280,10 +280,8 @@ void collision(const t_param params, t_speed* cells, t_speed* tmp_cells, int* ob
   ** NB the collision step is called after
   ** the propagate step and so values of interest
   ** are in the scratch-space grid */
-// #pragma omp parallel for schedule(dynamic,1) reduction(+: tot_u, tot_cells)
 
-
-#pragma omp parallel for simd reduction(+:tot_cells,tot_u) schedule(static) num_threads(16)
+#pragma omp parallel for simd reduction(+:tot_cells,tot_u) schedule(static) num_threads(16) 
 for (int ii = 0; ii < params.ny; ii++)
   {
       int y_s = (ii == 0) ? (ii + params.ny - 1) : (ii - 1); // could move up
@@ -706,6 +704,3 @@ void usage(const char* exe)
 
 
 
-
-// d2q9-bgk input_128x128.params obstacles_128x128.dat
-//     printf("value: %d==\n", tt);
