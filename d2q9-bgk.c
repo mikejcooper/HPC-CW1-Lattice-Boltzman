9db945e9ede05dbea4e7a36dea04b544e09961f0
Mike Cooper <mikecooper@Mikes-Mac.local>
2016-10-21 09:11:34 +0100

    test

--- d2q9-bgk.c
+++ d2q9-bgk.c
@@ -343,7 +343,7 @@ for (int ii = 0; ii < params.ny; ii++)
                       - (cells[y_n * params.nx + jj].speeds[4]
                          + cells[y_n * params.nx + x_e].speeds[7]
                          + cells[y_n * params.nx + x_w].speeds[8]))
-                     * local_density_invert;
+                     * local_density_invert; 
 
  
 
@@ -362,16 +362,16 @@ for (int ii = 0; ii < params.ny; ii++)
                                                     - cells[y_s * params.nx + jj].speeds[2]);
         tmp_cells[index].speeds[3] = cells[ii * params.nx + x_e].speeds[3]
                                                   + params.omega
-                                                  * (local_density * d1 * (4 + - u_x * 12 + (u_x * u_x) * 648 * d1 - (216 * d1 * (u_x * u_x + u_y * u_y)))
+                                                  * (local_density * d1 * (4 + - u_x * 12 + (- u_x * - u_x) * 648 * d1 - (216 * d1 * (u_x * u_x + u_y * u_y)))
                                                     - cells[ii * params.nx + x_e].speeds[3]);
         tmp_cells[index].speeds[4] = cells[y_n * params.nx + jj].speeds[4]
                                                   + params.omega
-                                                  * (local_density * d1 * (4 + - u_y * 12 + (u_y * u_y) * 648 * d1 - (216 * d1 * (u_x * u_x + u_y * u_y)))
+                                                  * (local_density * d1 * (4 + - u_y * 12 + (- u_y * - u_y) * 648 * d1 - (216 * d1 * (u_x * u_x + u_y * u_y)))
                                                     - cells[y_n * params.nx + jj].speeds[4]);
         tmp_cells[index].speeds[5] = cells[y_s * params.nx + x_w].speeds[5]
                                                   + params.omega
                                                   * (local_density * d1 * (1 + (u_x + u_y) * 3 + ((u_x + u_y) * (u_x + u_y)) * 162 * d1 - (54 * d1 * (u_x * u_x + u_y * u_y)))
-                                                    - cells[y_s * params.nx + x_w].speeds[5]); 
+                                                    - cells[y_s * params.nx + x_w].speeds[5]);
         tmp_cells[index].speeds[6] = cells[y_s * params.nx + x_e].speeds[6]
                                                   + params.omega
                                                   * (local_density * d1 * (1 + (- u_x + u_y) * 3 + ((- u_x + u_y) * (- u_x + u_y)) * 162 * d1 - (54 * d1 * (u_x * u_x + u_y * u_y)))
@@ -384,7 +384,6 @@ for (int ii = 0; ii < params.ny; ii++)
                                                   + params.omega
                                                   * (local_density * d1 * (1 + (u_x - u_y) * 3 + ((u_x - u_y) * (u_x - u_y)) * 162 * d1 - (54 * d1 * (u_x * u_x + u_y * u_y)))
                                                     - cells[y_n * params.nx + x_w].speeds[8]);
-                                                  // 
 
 // --------------av_velocity-----------------------------------------------
 
